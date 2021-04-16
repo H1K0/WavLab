60c09c0f93cb45c26aa4b4a43c40909e9d9240d8
H1K0 <hiko-19@ya.ru>
2021-04-16 11:07:14 +0300

    Introducing amplitude modulation

--- SounDier.py
+++ SounDier.py
@@ -1,11 +1,12 @@
 from WavZard import Wav
 from MatHero import db
-from math import sin, pi
+from math import sin, cos, pi
 from numpy import array as arr, hstack as seq
 
 
 class SounDier:
     """So, here we go working with the sound."""
+
     def __init__(self, wav):
         self.wavfile = wav
         self.channels = self.wavfile.channels
@@ -13,44 +14,53 @@ class SounDier:
         self.samprate = self.wavfile.samprate
 
     def sine(self, freq, amp, dur):
-        return arr([int(amp * 2 ** (self.bitdepth - 1) * sin(2 * freq * pi * i / self.samprate))
-                    for i in range(round(dur * self.samprate))], dtype=f'int{self.bitdepth}')
+        """Generates sine wave: [freq] hertz, [amp] relative amplitude (0<=[amp]<=1), [dur] secs."""
+        return arr([int(amp * 2 ** (self.bitdepth - 1) * sin(freq * 2 * pi * s / self.samprate))
+                    for s in range(round(dur * self.samprate))], dtype=f'int{self.bitdepth}')
 
     def silence(self, dur):
+        """Generates [dur] secs silence."""
         return arr([0 for _ in range(round(dur * self.samprate))], dtype=f'int{self.bitdepth}')
+
+    def am(self, carr, modfunc, offset=0):
+        """Applies amplitude modulation to [carr] by [mod] with [offset] phase offset (in samples)."""
+        mod = arr([modfunc(s + offset) for s in range(len(carr))])
+        return arr(carr * mod, dtype=f'int{self.bitdepth}')
 
     def write(self, *data):
         self.wavfile.write(seq([*data]))
 
 
-def telephone(num,vol=db(-8)):
+def telephone(num, vol=db(-8)):
     """Convert some symbols to DTMF."""
     global sound
-    num=num.upper()
-    dtmf=sound.silence(.1)
+    num = num.upper()
+    dtmf = sound.silence(.1)
     for n in num:
-        this=sound.silence(.08)
-        if n in ('1','2','3','A'):
-            this+=sound.sine(697,vol,.08)
-        if n in ('4','5','6','B'):
-            this+=sound.sine(770,vol,.08)
-        if n in ('7','8','9','C'):
-            this+=sound.sine(852,vol,.08)
-        if n in ('*','0','#','D'):
-            this+=sound.sine(941,vol,.08)
-        if n in ('1','4','7','*'):
-            this+=sound.sine(1209,vol,.08)
-        if n in ('2','5','8','0'):
-            this+=sound.sine(1336,vol,.08)
-        if n in ('3','6','9','#'):
-            this+=sound.sine(1477,vol,.08)
-        if n in ('A','B','C','D'):
-            this+=sound.sine(1633,vol,.08)
-        dtmf=seq([dtmf,this,sound.silence(.1)])
+        this = sound.silence(.08)
+        if n in ('1', '2', '3', 'A'):
+            this += sound.sine(697, vol, .08)
+        if n in ('4', '5', '6', 'B'):
+            this += sound.sine(770, vol, .08)
+        if n in ('7', '8', '9', 'C'):
+            this += sound.sine(852, vol, .08)
+        if n in ('*', '0', '#', 'D'):
+            this += sound.sine(941, vol, .08)
+        if n in ('1', '4', '7', '*'):
+            this += sound.sine(1209, vol, .08)
+        if n in ('2', '5', '8', '0'):
+            this += sound.sine(1336, vol, .08)
+        if n in ('3', '6', '9', '#'):
+            this += sound.sine(1477, vol, .08)
+        if n in ('A', 'B', 'C', 'D'):
+            this += sound.sine(1633, vol, .08)
+        dtmf = seq([dtmf, this, sound.silence(.1)])
     return dtmf
 
 
-sound = SounDier(Wav('WAV/test.wav', channels=1, bitdepth=16, samprate=44100))
-sound.write(
-    telephone('2281488')
-)
+if __name__ == '__main__':
+    sound = SounDier(Wav('WAV/test.wav', channels=1, bitdepth=16, samprate=44100))
+    am = lambda s: db(-6) + db(-12) * cos(500 * 2 * pi * s / 44100)
+    sound.write(
+        sound.am(sound.sine(440, db(0), 5), am)
+    )
